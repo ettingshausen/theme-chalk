6313ddcafbb09ef72568b6a0f45e89e9a4e168d9
ettingshausen <ettingshausen@outlook.com>
2020-04-03 16:51:36 +0800

    Autoprefixer warning handle

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,13 @@
 
 const { series, src, dest } = require('gulp');
 const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const autoprefixer = require('autoprefixer');
 const cssmin = require('gulp-cssmin');
-
+const postcss = require('gulp-postcss')
 function compile() {
   return src('./src/*.scss')
     .pipe(sass.sync())
-    .pipe(autoprefixer({
-      browsers: ['ie > 9', 'last 2 versions'],
-      cascade: false
-    }))
+    .pipe(postcss([autoprefixer()]))
     .pipe(cssmin())
     .pipe(dest('./lib'));
 }
